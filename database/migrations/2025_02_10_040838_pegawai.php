347b62d541fd233e5599c9830644a2613791d913
AISYAH SITI ZAHRA <160479611+aszahra@users.noreply.github.com>
2025-02-10 11:32:29 +0700

    add migration

---
+++ database/migrations/2025_02_10_040838_pegawai.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->string('kode_jabatan');
+            $table->string('nama');
+            $table->string('jenis_kelamin');
+            $table->date('tanggal_lahir');
+            $table->string('no_hp');
+            $table->text('alamat');
+            $table->string('status_pegawai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
